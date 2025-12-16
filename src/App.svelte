fc2596264f9765898215649fcd67b1984443f9df
Jacob Strieb <jstrieb@users.noreply.github.com>
2025-12-16 00:42:31 -0500

    Re-parse in effects

--- src/App.svelte
+++ src/App.svelte
@@ -38,10 +38,41 @@
 
 <script>
   import { formula } from "./formula.js";
+  import { ParseError } from "./lib/parsers.js";
+
+  class Cell {
+    constructor(formula) {
+      this.formula = $state(formula);
+      this.value = $state(undefined);
+    }
+
+    toString() {
+      return JSON.stringify(this.value);
+    }
+  }
 
   const rows = $state(
-    new Array(3).fill().map(() => new Array(3).fill().map(() => null)),
+    new Array(3).fill().map(() => new Array(3).fill().map(() => new Cell())),
   );
+
+  $effect(() => {
+    for (const row of rows) {
+      for (const cell of row) {
+        $effect(() => {
+          try {
+            cell.value = formula.parse(cell.formula);
+          } catch (e) {
+            if (e instanceof ParseError) {
+              cell.value = cell.formula;
+            } else {
+              console.error(e);
+              cell.value = e;
+            }
+          }
+        });
+      }
+    }
+  });
 </script>
 
 <div class="container">
@@ -64,20 +95,12 @@
                 <input
                   type="text"
                   style="border-bottom: 1px dashed lightgray;"
-                  bind:value={rows[i][j]}
+                  bind:value={rows[i][j].formula}
                 />
                 <p
                   style="padding: 0.25rem; min-height: 1.5rem; white-space: pre;"
                 >
-                  {#if rows[i][j]}
-                    {(() => {
-                      try {
-                        return JSON.stringify(formula.parse(rows[i][j]));
-                      } catch {
-                        return rows[i][j];
-                      }
-                    })()}
-                  {/if}
+                  {(rows[i][j].value && rows[i][j]) || ""}
                 </p>
               </div>
             </td>
