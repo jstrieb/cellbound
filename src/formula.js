df179527c7e654e5a9a492cb29c088a26a687386
Jacob Strieb <jstrieb@users.noreply.github.com>
2025-12-19 00:59:16 -0500

    Compute unary and binary operations

--- src/formula.js
+++ src/formula.js
@@ -11,6 +11,7 @@ import {
   infixL,
   regex,
 } from "./lib/parsers.js";
+import { derived } from "svelte/store";
 
 ///////////////////////////////////////////////////////////////////////////////
 // Classes
@@ -25,6 +26,10 @@ class Variable extends Expression {
     super();
     this.name = name;
   }
+
+  compute(cells, row, col, variables) {
+    return variables[name];
+  }
 }
 
 class Function extends Expression {
@@ -35,6 +40,10 @@ class Function extends Expression {
     super();
     this.name = name;
     this.args = Array.from(args);
+  }
+
+  compute(cells, row, col, variables) {
+    throw new Error("Not yet implemented");
   }
 }
 
@@ -52,6 +61,18 @@ class UnaryOp extends Expression {
     super();
     this.operator = operator;
     this.operand = operand;
+  }
+
+  compute(...args) {
+    const { operand, operator } = this;
+    const computed = operand.compute ? operand.compute(...args) : operand;
+    if (computed.subscribe) {
+      return derived([computed], (x, set) =>
+        set(UnaryOp.operations[operator](x)),
+      );
+    } else {
+      return UnaryOp.operations[operator](computed);
+    }
   }
 }
 
@@ -85,6 +106,47 @@ class BinaryOp extends Expression {
     super();
     this.ast = Array.from(ast);
   }
+
+  compute(...args) {
+    const ast = [...this.ast];
+    if (ast.length < 3) {
+      console.log(ast);
+      throw new Error("Binary operation AST has incorrect length");
+    }
+
+    while (ast.length > 1) {
+      if (ast.length % 2 != 1) {
+        console.log(ast);
+        throw new Error("Binary operation AST length not odd");
+      }
+      const _x = ast.shift();
+      const x = _x.compute ? _x.compute(...args) : _x;
+      const op = ast.shift();
+      const _y = ast.shift();
+      const y = _y.compute ? _y.compute(...args) : _y;
+      const isXStore = x?.subscribe;
+      const isYStore = y?.subscribe;
+
+      if (isXStore && isYStore) {
+        ast.unshift(
+          derived([x, y], ([a, b], set) => set(BinaryOp.operations[op](a, b))),
+        );
+      } else if (isXStore) {
+        ast.unshift(
+          derived([x], ([a], set) => set(BinaryOp.operations[op](a, y))),
+        );
+      } else if (isYStore) {
+        ast.unshift(
+          derived([y], ([b], set) => set(BinaryOp.operations[op](x, b))),
+        );
+      } else {
+        ast.unshift(BinaryOp.operations[op](x, y));
+      }
+    }
+
+    const [computed] = ast;
+    return computed;
+  }
 }
 
 class Reference extends Expression {
@@ -95,6 +157,10 @@ class Reference extends Expression {
     super();
     this.row = row;
     this.col = col;
+  }
+
+  compute(cells, row, col, variables) {
+    throw new Error("Not yet implemented");
   }
 }
 
@@ -110,6 +176,10 @@ class Range extends Expression {
     this.startCol = startCol;
     this.endRow = endRow;
     this.endCol = endCol;
+  }
+
+  compute(cells, row, col, variables) {
+    throw new Error("Not yet implemented");
   }
 }
 
