0097177e640be47aec1ae2ad39d2b0c0b5a6974d
Jacob Strieb <jstrieb@users.noreply.github.com>
2025-12-19 22:31:36 -0500

    Add common formula functions

--- src/formula-functions.js
+++ src/formula-functions.js
@@ -23,3 +23,28 @@ functions.tick = function (ms) {
   setInterval(() => this.update((ticks) => ticks + 1), ms);
   return 0;
 };
+
+functions.avg = (...args) =>
+  args.flat(Infinity).reduce((a, x) => a + x ?? 0, 0) /
+  args.flat(Infinity).length;
+
+functions.randint = (n) => Math.floor(Math.random() * n);
+functions.if = (x, yes, no) => (x ? yes : no);
+
+// Aliases
+functions.add = functions.sum;
+functions.plus = functions.sum;
+functions.times = functions.prod;
+functions.product = functions.prod;
+functions.mult = functions.prod;
+functions.average = functions.avg;
+functions.rand = functions.random;
+
+functions.sparkbars = (...args) => {
+  args = args.flat(Infinity).map((x) => x ?? 0);
+  const lines = "▁▂▃▄▅▆▇█";
+  const min = Math.min(...args),
+    max = Math.max(...args);
+  const bucketSize = (max - min) / (lines.length - 1);
+  return args.map((x) => lines[Math.floor((x - min) / bucketSize)]).join("");
+};
